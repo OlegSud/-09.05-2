fc0ce367d4ef0aefa5f9953bbb850e4d283fd1c4
OlegSud <168107881+OlegSud@users.noreply.github.com>
2024-05-09 22:06:31 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['village', 'house', 'sauna', 'barn', 'well']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2]= '8'
+print(my_list)
+
+my_dict = {'дача': 'village', 'дом': 'house', 'баня': 'sauna', 'сарай': 'barn', 'скважина': 'well'}
+print(my_dict)
+print(my_dict['скважина'])
+my_dict['город'] = 'harm'
+print(my_dict)
